13a495d85dfffc0941005c6a6e82df0e1442ff21
Restyled.io <commits@restyled.io>
2021-02-22 10:02:35 +0000

    Restyled by prettier

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,47 +1,54 @@
-import {Input, SIZE} from "baseui/input";
-import * as React from 'react';
-import {useSelector} from 'react-redux';
+import { Input, SIZE } from "baseui/input";
+import * as React from "react";
+import { useSelector } from "react-redux";
 
-import Note from './Note';
+import Note from "./Note";
 
-export default ({value}) => {
+export default ({ value }) => {
   const [notes, setNotes] = React.useState([]);
   const [query, setQuery] = React.useState("");
-  const userId = useSelector(state => state.userId);
+  const userId = useSelector((state) => state.userId);
 
   const searchNotes = (event) => {
-	setQuery(event.target.value);
-  }
+    setQuery(event.target.value);
+  };
 
   React.useEffect(() => {
-	const fetchNotes = async () => {
-	  try {
-		const url = `${process.env.REACT_APP_API_URL}/notes/u/${userId}`;
-		const res = await fetch(url);
-		const data = await res.json();
-		setNotes(data.data.notes.map(note => note));
-	  } catch (error) {
-		console.error(error.stack);
-	  }
-	}
-	fetchNotes();
+    const fetchNotes = async () => {
+      try {
+        const url = `${process.env.REACT_APP_API_URL}/notes/u/${userId}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        setNotes(data.data.notes.map((note) => note));
+      } catch (error) {
+        console.error(error.stack);
+      }
+    };
+    fetchNotes();
   }, [userId]);
 
   return (
-	<>
-	  <Input
-		value={value}
-		size={SIZE.large}
-		onChange={searchNotes}
-		placeholder="Controlled Input"
-		clearable
-	  />
-	  <div className="notes-list-container">
-		{
-		  notes.map(note => note).filter(note => note.title.includes(query)).map(note => <Note key={note.id} className="note-item" note={note} title={note.title}/>)
-		}
-	  </div>
-	</>
+    <>
+      <Input
+        value={value}
+        size={SIZE.large}
+        onChange={searchNotes}
+        placeholder="Controlled Input"
+        clearable
+      />
+      <div className="notes-list-container">
+        {notes
+          .map((note) => note)
+          .filter((note) => note.title.includes(query))
+          .map((note) => (
+            <Note
+              key={note.id}
+              className="note-item"
+              note={note}
+              title={note.title}
+            />
+          ))}
+      </div>
+    </>
   );
-}
-
+};
